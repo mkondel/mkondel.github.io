072f44b7e2e961e1328ece136a9b1ff03b6d58e5
max <mkondel@gmail.com>
2015-04-18 15:50:43 -0400

    it\s doing something

--- basic_dodo.js
+++ basic_dodo.js
@@ -8,24 +8,25 @@ if (typeof DODO === 'undefined') DODO = {};
     input.select1 = options.select1
     input.select2 = options.select2
     input.optimize = options.optimize
-    input.notes_in_order = ['C','c','D','d','E','F','f','G','g','A','a','B']
+    // input.notes_in_order = ['C','c','D','d','E','F','f','G','g','A','a','B']
 
     // return random integer with 0 ≤ j ≤ n
     input.j = function(n){return Math.floor(Math.random() * (n+1));}
 
     // Sattolo's To shuffle an array a of n elements (indices 0..n-1):
     input.unique_set_one_of_each = function(a){
-      var n = a.length
+      var b = a.slice()
+      var n = b.length
       //for i from n − 1 downto 1 do
       for(var i=n-1; i>1; i--){
         //j ← random integer with 0 ≤ j ≤ i
         var J = this.j(i)
         //exchange a[j] and a[i]
-        var temp = a[i]
-        a[i] = a[J]
-        a[J] = temp
+        var temp = b[i]
+        b[i] = b[J]
+        b[J] = temp
       }
-      return a
+      return b
     }
 
     // To shuffle an array a of n elements (indices 0..n-1):
@@ -37,14 +38,17 @@ if (typeof DODO === 'undefined') DODO = {};
       return a
     }
 
+    // input.notes_in_order = input.unique_set_one_of_each(['C','c','D','d','E','F','f','G','g','A','a','B'])
+    input.notes_in_order = ['C','c','D','d','E','F','f','G','g','A','a','B']
+
     input.seed = function() {
+      // console.log('seed: '+this.notes_in_order)
       var entity = this.unique_set_one_of_each(this.notes_in_order)
-      // console.log('seed '+ entity)
       return entity
     }
 
     input.mutate = function(entity) {
-      var mutant = entity
+      var mutant = entity.slice()
       var a=Math.floor(Math.random() * mutant.length)
       var b=Math.floor(Math.random() * this.notes_in_order.length)
       mutant[a] = this.notes_in_order[b]
@@ -53,25 +57,29 @@ if (typeof DODO === 'undefined') DODO = {};
 
     // one-point crossover
     input.crossover0 = function(mother, father){
-      var son=mother, daughter=father, x=Math.floor(Math.random() * mother.length)
+      var son=mother,
+          daughter=father,
+          x=Math.floor(Math.random() * mother.length)
+
       son = [].concat(mother.slice(0,x),father.slice(x))
-      daughter = son.reverse()
+      daughter = son.slice().reverse()
       return [son, daughter]
     }
 
     // two-point crossover
     input.crossover = function(mother, father) {
       var len = mother.length;
-      var ca = Math.floor(Math.random()*len);
-      var cb = Math.floor(Math.random()*len);   
-      if (ca > cb) {
-        var tmp = cb;
-        cb = ca;
-        ca = tmp;
+      var start = Math.floor(Math.random()*len);
+      var end = Math.floor(Math.random()*len); 
+      // console.log(start, end)  
+      if (start > end) {
+        var tmp = end;
+        end = start;
+        start = tmp;
       }
         
-      var son = [].concat(father.slice(0,ca), mother.slice(ca, cb-ca), father.slice(cb))
-      var daughter = [].concat(mother.slice(0,ca), father.slice(ca, cb-ca), mother.slice(cb))
+      var son = [].concat(father.slice(0,start), mother.slice(start, end), father.slice(end))
+      var daughter = [].concat(mother.slice(0,start), father.slice(start, end), mother.slice(end))
       
       return [son, daughter];
     }
@@ -79,23 +87,26 @@ if (typeof DODO === 'undefined') DODO = {};
     input.fitness = function(entity) {
       var diff = [], sum=0
       for(var i=0; i<entity.length; i++){
-        diff.push(this.notes_in_order.indexOf(entity[i])-i)
+        diff.push(this.notes_in_order.indexOf(entity[i]))
       }
 
       for(var i=0; i<diff.length; i++){
-        sum += Math.abs(diff[i])*Math.pow(2,i)
+        sum += Math.pow(1.001,diff[i]-i)
       }
-      return sum
+      return (sum-entity.length)*100000
     }
 
-    input.generation = function(pop, generation, stats) {}
+    input.generation = function(pop, generation, stats) {
+    }
 
     input.notification = function(pop, generation, stats, isFinished) {
       if(isFinished){
+        console.log('DODOKEY: '+input.notes_in_order)
         console.log('Best after '+(generation+1)+' generations: '+JSON.stringify(pop[0]))
-        // console.log('Worst after '+(generation+1)+' generations: '+JSON.stringify(pop[pop.length-1]))
+        console.log('Worst after '+(generation+1)+' generations: '+JSON.stringify(pop[pop.length-1]))
       }else{
-        console.log('best in '+generation+'_th generation is'+JSON.stringify(pop[0]))
+        // console.log('best in '+generation+'_th generation is '+JSON.stringify(pop[0]))
+        // console.log('worst in '+generation+'_th generation is '+JSON.stringify(pop[pop.length-1]))
       }
     }
 
