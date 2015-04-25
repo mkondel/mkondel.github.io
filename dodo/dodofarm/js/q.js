7f9d47bfbec3e77c6cd201f5903b7b8c91b61c22
max <mkondel@gmail.com>
2015-04-24 20:46:18 -0400

    input

--- dodo/dodofarm/js/q.js
+++ dodo/dodofarm/js/q.js
@@ -4,6 +4,33 @@ self.printme = function(m){typeof console!='undefined'?console.log(m):null}
 
 var choice = ['A','B']
 ,   j = function(n){return Math.floor(Math.random() * (n+1))}
+,   get_ascii = function(x){ return PLAY.note_phrase_ascii($('#'+x).html().split(',')).join('') }
+,   chose = function(c){
+      var dics = {A:['A','B'], B:['B','A']}
+      PLAY.play_ascii( get_ascii(c).split('') )
+      return dics[c]
+    }
+,   save = function(c){
+      var   da = get_ascii(c[0])
+      ,   nyet = get_ascii(c[1])
+      ,   hash = CryptoJS.SHA3(da+nyet, { outputLength: 64 })
+
+      localStorage.setItem(hash, JSON.stringify( { yes:da, no:nyet } ))
+    }
+,   evolve = function(c){
+      var   da = get_ascii(c[0])
+      ,   nyet = get_ascii(c[1])
+      ,   hash = CryptoJS.SHA3(da+nyet, { outputLength: 64 })
+
+      alert('this is input: '+JSON.stringify({yes:da, no:nyet}, null, 2))
+    }
+,   n_completely_random = function(alphabet, n){
+        var a = []
+        for(var i=n; i>0; i--){
+          a.push(alphabet[j(alphabet.length-1)])
+        }
+        return a.join('')
+      }
 ,   unique_set_one_of_each = function(a){
         var b = a.slice()
         var n = b.length
@@ -18,28 +45,6 @@ var choice = ['A','B']
         }
         return b
       }
-,   n_completely_random = function(alphabet, n){
-        var a = []
-        for(var i=n; i>0; i--){
-          a.push(alphabet[j(alphabet.length-1)])
-        }
-        return a.join('')
-      }
-
-,   chose = function(c){
-      var dics = {A:['A','B'], B:['B','A']}
-      PLAY.play_ascii( get_ascii(c).split('') )
-      return dics[c]
-    }
-,   get_ascii = function(x){ return PLAY.note_phrase_ascii($('#'+x).html().split(',')).join('') }
-,   store_notes = function(c){
-      var   da = get_ascii(c[0])
-      ,   nyet = get_ascii(c[1])
-      ,   hash = CryptoJS.SHA3(da+nyet, { outputLength: 64 })
-
-      localStorage.setItem(hash, JSON.stringify( { yes:da, no:nyet } ))
-    }
-
 
 $('.seeder').focus()
   .bind('keypress', function(e) {
@@ -48,19 +53,19 @@ $('.seeder').focus()
       $('#A').html(PLAY.ascii_phrase_notes( user_notes ).join(','))
       $('#B').html(PLAY.ascii_phrase_notes( unique_set_one_of_each(user_notes) ).join(','))
       $('.asong').fadeIn(1000)
-      $('.save').fadeIn(3000)
+      $('.controls').fadeIn(2000)
   })
 
 $('#A').attr('title','Song A')
   .on('click',function(){ choice = chose('A') })
-
 $('#B').attr('title','Song B')
   .on('click',function(){ choice = chose('B') })
-
-$('.save').hide()
+$('#save')
   .attr('title','Save choice')
-  .on('click',function(){ store_notes(choice) })
-
+  .on('click',function(){ save(choice) })
+$('#evolve')
+  .attr('title','Evolve chosen')
+  .on('click',function(){ evolve(choice) })
 
 
 $.when(
